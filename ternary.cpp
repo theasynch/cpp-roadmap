1ed465c30378c1578eaaed9758d151e68c0f6c38
Om Awthankar <92038575+theasynch@users.noreply.github.com>
2025-12-28 18:11:14 +0530

    Ternary Operator
    Understanding the equivalent of one line if-else statements from Python in C++.

---
+++ ternary.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+    //Ternary operators are like one line if-else statements from python
+
+    /*  Example:
+    marks = int(input("Enter your marks: "))
+    print("Pass" if marks >= 40 else "Fail") <---- equivalent of ternary operator in python    */
+
+    /* Syntax of a ternary operator in C++:
+    condition ? expression_if_true : expression_if_false; */
+
+
+    int marks;
+    std::cout << "Enter your marks: ";
+    std::cin >> marks;
+    marks >=50 ? std::cout << "You passed the exam. "  : std::cout << "You failed the exam. ";
+    std::string grade = (marks >= 40) ? "A" : "F"; //Assigning value using ternary operator
+    std::cout << "Your grade is " << grade;
+
+}
