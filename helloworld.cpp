5b3d0666b8e5725af01f1f6e21f5af139a4c7ce5
Om Awthankar <92038575+theasynch@users.noreply.github.com>
2025-12-28 15:17:31 +0530

    std::cout statement
    Understanding how to print something.

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    //Basic printing statements.
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "This program is compiled using C++17 standard." << std::endl;
+
+
+    return 0;
+}
