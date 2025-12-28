3a9a1ec77b30c705d43a71e351815aa89ec7f5bf
Om Awthankar <92038575+theasynch@users.noreply.github.com>
2025-12-28 15:19:20 +0530

    Understanding variable declaration

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,6 +5,10 @@ int main() {
     std::cout << "Hello World!" << std::endl;
     std::cout << "This program is compiled using C++17 standard." << std::endl;
 
-
+    //Understanding variable declaration and initialization
+    int x = 5;
+    int y = 10;
+    int sum = x + y;
+    std::cout << "The sum of " << x << " and " << y << " is " << sum << "." << std::endl;
     return 0;
 }
